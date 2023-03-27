81e0720e65b1fde1300402c363898d9a9b16bc3a
bakhacode <bakhamitai@naver.com>
2023-03-27 17:58:02 +0900

    docs: 리드미 수정
    간단하게 순서를 나눠서 계획을 수립했다.

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 const createTown = document.createElement("div");
 
 document.addEventListener("click", (e) => {
-  if (e.target.tagName === "button") {
-  }
+  //   if (e.target.tagName === "button") {
+  //   }
+  //   traverse(document);
+  console.log(document);
+  console.log(typeof document);
   //   console.log(e.target.tagName);
 });
 
-document.addEventListener("DOMContentLoaded");
+document.addEventListener("DOMContentLoaded", () => {});
+
+const a = `
+    <div style="left: 100px; width: 100px; height: 100px"></div>
+`;
+
+function traverse(node) {
+  if (node.nodeType === Node.ELEMENT_NODE) {
+    console.log(node.nodeName);
+  }
+  node = node.firstChild;
+  while (node) {
+    traverse(node);
+    node = node.nextSibling;
+  }
+}
