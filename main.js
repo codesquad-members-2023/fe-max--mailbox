909a0c508fb9dc9c8e2ea616966ec2d869d8093d
bakhacode <bakhamitai@naver.com>
2023-03-27 15:20:28 +0900

    feat: 로딩시 변경이 없는 메인페이지 html,css 구현
    간단하게 html과 css로 기본 틀을 먼저 잡았고, 버튼에 이벤트리스너만 달아두었다.

---
+++ main.js
@@ -0,0 +1,9 @@
+const createTown = document.createElement("div");
+
+document.addEventListener("click", (e) => {
+  if (e.target.tagName === "button") {
+  }
+  //   console.log(e.target.tagName);
+});
+
+document.addEventListener("DOMContentLoaded");
