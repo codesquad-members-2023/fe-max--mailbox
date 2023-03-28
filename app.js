059273bcaa2f3acd7b6e06ebfde345c302725ca3
silvertae <massenger7@gmail.com>
2023-03-28 23:30:52 +0900

    feat: myQuerySelector와 myQuerySelectorAll 함수를 구현
    기존에 깊이 우선 탐색 알고리즘을 기반으로 만들어둔 함수들을 활용해서
    document.querySelector()처럼 css 선택자를 인자로 받아서 일치하는 요소나
    요소들이 담긴 배열을 반환하는 myQuerySelector와 myQuerySelectorAll
    함수를 구현했습니다.

--- app.js
+++ app.js
@@ -1,43 +1,88 @@
-// function myQuerySelector() {
+const button = myQuerySelector('button');
+button.addEventListener('click', () => {
+	console.log('clicked!');
+});
 
-// }
+function myQuerySelector(selector) {
+	const selectorName = selector.substr(1);
+	if (selector.at(0) === '#') {
+		return findNodeById(selectorName);
+	} else if (selector.at(0) === '.') {
+		return findNodeByClassName(selectorName);
+	} else {
+		return findNodeByTagName(selector);
+	}
+}
+
+function myQuerySelectorAll(selector) {
+	const selectorName = selector.substr(1);
+	if (selector.at(0) === '#') {
+		return [findNodeById(selectorName)];
+	} else if (selector.at(0) === '.') {
+		return findNodeByClassNameAll(selectorName);
+	} else {
+		return findNodeByTagNameAll(selector);
+	}
+}
 
 function walkPreOrder(node, nodes = []) {
-  if (!node) return nodes;
+	if (!node) return nodes;
 
-  nodes.push(node);
-  for (const child of node.children) {
-    walkPreOrder(child, nodes);
-  }
-  return nodes;
+	nodes.push(node);
+	for (const child of node.children) {
+		walkPreOrder(child, nodes);
+	}
+	return nodes;
 }
 
 function findNodeById(nodeId) {
-  const DOMTree = walkPreOrder(document.body);
-  for (const node of DOMTree) {
-    if (node.id === nodeId) {
-      return node;
-    }
-  }
-  return null;
+	const DOMTree = walkPreOrder(document.body);
+	for (const node of DOMTree) {
+		if (node.id === nodeId) {
+			return node;
+		}
+	}
+	return null;
 }
 
 function findNodeByTagName(tag) {
-  const DOMTree = walkPreOrder(document.body);
-  for (const node of DOMTree) {
-    if (node.tagName === tag.toUpperCase()) {
-      return node;
-    }
-  }
-  return null;
+	const DOMTree = walkPreOrder(document.body);
+	for (const node of DOMTree) {
+		if (node.tagName === tag.toUpperCase()) {
+			return node;
+		}
+	}
+	return null;
+}
+
+function findNodeByTagNameAll(tag) {
+	const DOMTree = walkPreOrder(document.body);
+	const nodes = [];
+	for (const node of DOMTree) {
+		if (node.tagName === tag.toUpperCase()) {
+			nodes.push(node);
+		}
+	}
+	return nodes;
 }
 
 function findNodeByClassName(className) {
-  const DOMTree = walkPreOrder(document.body);
-  for (const node of DOMTree) {
-    if (node.classList.contains(className)) {
-      return node;
-    }
-  }
-  return null;
+	const DOMTree = walkPreOrder(document.body);
+	for (const node of DOMTree) {
+		if (node.classList.contains(className)) {
+			return node;
+		}
+	}
+	return null;
+}
+
+function findNodeByClassNameAll(className) {
+	const DOMTree = walkPreOrder(document.body);
+	const nodes = [];
+	for (const node of DOMTree) {
+		if (node.classList.contains(className)) {
+			nodes.push(node);
+		}
+	}
+	return nodes;
 }
