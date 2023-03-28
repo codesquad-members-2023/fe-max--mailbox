818748ba4c19a7dbdd97043d4fe07e44d1c0f62a
Jisoo Yoo <youzysu@gmail.com>
2023-03-28 18:32:19 +0900

    feat: 마을 정보를 받아 화면에 렌더링한다.

--- main.js
+++ main.js
@@ -4,7 +4,25 @@ function main() {
   const root = document.querySelector('root');
 
   const towns = villageMaker();
+  makeVillageView(towns);
   console.log(towns);
 }
+
+const makeVillageView = (towns) => {
+  const main = document.querySelector('.root');
+  const townsView = towns.map(createTownNode);
+  main.append(...townsView);
+};
+
+const createTownNode = (town) => {
+  const element = document.createElement('div');
+  element.classList.add('town', town.name);
+  element.style.width = `${town.width}px`;
+  element.style.height = `${town.height}px`;
+  element.style.top = `${town.pointY}px`;
+  element.style.left = `${town.pointX}px`;
+  element.style.position = 'absolute';
+  return element;
+};
 
 main();
