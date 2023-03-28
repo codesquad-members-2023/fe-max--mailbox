cf8a06881a3af11913835dff5071b1288337c989
bakhacode <bakhamitai@naver.com>
2023-03-28 19:39:37 +0900

    feat: 마을지도 안에 마을을 여러개 생성하는 함수 구현
    임의의 반복 횟수를 정해서 일단 마을이 생성이 잘 되는지 확인을 위한 함수를 구현했다.

--- main.js
+++ main.js
@@ -1,87 +1,98 @@
-const createTown = document.createElement("div");
+const map = mySelector(".map");
 
-document.addEventListener("click", (e) => {
-  
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-
-});
-
-let townNum = 1;
+let townNum = 0;
 
 function makeTown(outer) {
-  
-  const randomNum = Math.floor(Math.random() * (outer.offsetWidth / 4 + 1)) + 100;
-  let x = Math.floor(Math.random() * outer.offsetWidth * (3 / 4));
-  let y = Math.floor(Math.random() * outer.offsetHeight * (3 / 4));
+    townNum++;
+    let currentOuter = outer;
+    const randomNum =
+        Math.floor(Math.random() * (outer.offsetWidth / 4 + 1)) + 100;
+    let x = Math.floor(Math.random() * outer.offsetWidth * (3 / 4));
+    let y = Math.floor(Math.random() * outer.offsetHeight * (3 / 4));
 
-
-  return `
-  <div 
-    class="town" 
-    id="a"
-    style="position:absolu; 
-    top:${y}px;
-    left:${x}px;
-    width:${randomNum}px;
-    height:${randomNum}px;">
-    ${townNum}
-  </div>
-  `
+    outer.insertAdjacentHTML(
+        "afterbegin",
+        `
+    <div
+        class="town${townNum}"
+        style="position:relative;
+        top:${y}px;
+        left:${x}px;
+        width:${randomNum}px;
+        height:${randomNum}px;">
+        town${townNum}
+    </div>
+    `
+    );
+    currentOuter = mySelector(`.town${townNum}`);
+    // console.log(currentOuter);
+    return;
 }
 
-const map = mySelector(".map")
-
-map.insertAdjacentHTML("afterbegin" ,makeTown(map))
-
-function* walkPreOrder(node){
-  if(!node) return
-
-  // do something here
-  yield node
-  for(let child of node.children){
-    yield* walkPreOrder(child)
-  }
+function makeNum(string) {
+    return (num = parseInt(string));
 }
 
-
-function mySelector(selector){
-  const path = selector.split(' ').map(str => str.trim())
-
-  let currentNode = document.body
-  while(path.length && currentNode){
-
-    const currentSelector = path.shift()
-    let found = false
-
-    for(let node of walkPreOrder(currentNode)){
-      if(node.matches(currentSelector)){
-        currentNode = node
-        found = true
-        break
-      }
+function repeatMakeTown(outer) {
+    for (let i = 0; i < 3; i++) {
+        // if (outer.childNodes.length > 0) {
+        if (1) {
+            makeTown(outer);
+            console.log(outer.childNodes);
+            console.log("있따");
+            console.log(
+                makeNum(outer.firstElementChild.style.left) +
+                    makeNum(outer.firstElementChild.style.width)
+            );
+        } else {
+            console.log("업따");
+            makeTown(outer);
+        }
+        // makeTown(outer);
     }
-
-    if(!found) currentNode = null
-  }
-  return currentNode
 }
 
+map.getAttribute;
 
+repeatMakeTown(map);
 
+// map.insertAdjacentHTML("afterbegin", makeTown(map));
 
+function* walkPreOrder(node) {
+    if (!node) return;
 
+    // do something here
+    yield node;
+    for (let child of node.children) {
+        yield* walkPreOrder(child);
+    }
+}
+
+function mySelector(selector) {
+    const path = selector.split(" ").map((str) => str.trim());
+
+    let currentNode = document.body;
+    while (path.length && currentNode) {
+        const currentSelector = path.shift();
+        let found = false;
+
+        for (let node of walkPreOrder(currentNode)) {
+            if (node.matches(currentSelector)) {
+                currentNode = node;
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) currentNode = null;
+    }
+    return currentNode;
+}
 
 // body 요소의 위치를 랜덤한 값으로 설정합니다.
 
 // document.body.style.left = x + "px";
 // document.body.style.top = y + "px";
-
-
-
-
-
 
 // const item = document.querySelector('.item');
 // const container = document.querySelector('.container');
@@ -95,53 +106,64 @@ function mySelector(selector){
 // item.style.left = x * signX + 'px';
 // item.style.top = y * signY + 'px';
 
-
-
-
-
-
-
-
-
-
-
-
-
 const a = `
     <div style="left: 100px; width: 100px; height: 100px"></div>
 `;
 
 function traverse(node) {
-  if (node.nodeType === Node.ELEMENT_NODE) {
-    console.log(node.nodeName);
-  }
-  node = node.firstChild;
-  while (node) {
-    traverse(node);
-    node = node.nextSibling;
-  }
+    if (node.nodeType === Node.ELEMENT_NODE) {
+        console.log(node.nodeName);
+    }
+    node = node.firstChild;
+    while (node) {
+        traverse(node);
+        node = node.nextSibling;
+    }
 }
 
 // 하위 노드를 통한 재귀
 function eachNode(rootNode, callback) {
-  if (!callback) {
-    const nodes = [];
-    eachNode(rootNode, (node) => {
-      nodes.push(node);
-    });
-    return nodes;
-  }
-
-  if (callback(rootNode) === false) {
-    return false;
-  }
-
-  if (rootNode.hasChildNodes()) {
-    for (const node of rootNode.childNodes) {
-      if (eachNode(node, callback) === false) {
-        return;
-      }
+    if (!callback) {
+        const nodes = [];
+        eachNode(rootNode, (node) => {
+            nodes.push(node);
+        });
+        return nodes;
     }
-  }
+
+    if (callback(rootNode) === false) {
+        return false;
+    }
+
+    if (rootNode.hasChildNodes()) {
+        for (const node of rootNode.childNodes) {
+            if (eachNode(node, callback) === false) {
+                return;
+            }
+        }
+    }
 }
 
+// function setTown(outer) {
+//     const outerSize = outer.offsetWidth;
+//     const randomSize = Math.floor(Math.random() * (outerSize / 4)) + 100;
+//     let x = Math.floor(Math.random() * outerSize * (3 / 4));
+//     let y = Math.floor(Math.random() * outerSize * (3 / 4));
+
+//     for (let i = 0; i < 2; i++) {
+//         if (randomSize + x < outerSize && randomSize + y < outerSize) {
+//             return `
+//             <div
+//                 class="town"
+//                 id="a"
+//                 style="position:absolute;
+//                 top:${y}px;
+//                 left:${x}px;
+//                 width:${randomSize}px;
+//                 height:${randomSize}px;">
+//             </div>
+//             `;
+//         }
+//         setTown(outer);
+//     }
+// }
