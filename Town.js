0234c91e2f77e2f6771f20f9ca43a46e101dc11e
jay <chlwodnjs34@gmail.com>
2023-03-30 11:55:20 +0900

    refactor: 로직 전반적인 부분 개선
    - 부모와 겹치지 않는 경우에만 마을 프로퍼티 설정
    - static 변수를 정의하여 마을 인스턴스의 데이터 관리
    - 가독성을 위한 전반 수정
    - Town class, utils 모듈화

---
+++ Town.js
@@ -0,0 +1,61 @@
+import { makeRandomNumber } from './utils.js';
+import { makeVillage } from './makeVillage.js';
+
+export class Town {
+  static nameIndex = 65;
+  static mailboxSizeStorage = new Map();
+
+  constructor(parentWidth, parentHeight) {
+    this.SMALLEST_LENGTH = 100;
+    this.SMALLEST_SPACE = 10;
+    this.name = '';
+    this.width = this.makeRandomLength(parentWidth, this.SMALLEST_LENGTH);
+    this.height = this.makeRandomLength(parentHeight, this.SMALLEST_LENGTH);
+    this.pointX = this.makeRandomPoint(parentWidth, this.width);
+    this.pointY = this.makeRandomPoint(parentHeight, this.height);
+    this.children = [];
+    this.hasMailbox = Math.random() < 0.4;
+    this.mailboxSize = 0;
+  }
+
+  setTown() {
+    this.name = String.fromCharCode(Town.nameIndex++);
+    this.makeChildren();
+    this.setMailboxSize();
+  }
+
+  setMailboxSize() {
+    const MAX_MAILBOX_SIZE = 100;
+    const MIN_MAILBOX_SIZE = 1;
+
+    let randomSize = makeRandomNumber(MIN_MAILBOX_SIZE, MAX_MAILBOX_SIZE);
+
+    while (Town.mailboxSizeStorage.has(randomSize)) {
+      randomSize = makeRandomNumber(MIN_MAILBOX_SIZE, MAX_MAILBOX_SIZE);
+    }
+
+    Town.mailboxSizeStorage.set(randomSize, this.name);
+    this.mailboxSize = randomSize;
+  }
+
+  makeChildren() {
+    if (this.width <= this.SMALLEST_LENGTH || this.height <= this.SMALLEST_LENGTH) {
+      return;
+    }
+    this.children = makeVillage(this.width, this.height);
+  }
+
+  makeRandomLength(parentLength, smallestLength) {
+    const maxLength = parentLength / 2 - smallestLength;
+    const minLength = smallestLength - this.SMALLEST_SPACE;
+
+    return makeRandomNumber(minLength, maxLength);
+  }
+
+  makeRandomPoint(parentLength, distance) {
+    const maxPoint = parentLength - this.SMALLEST_SPACE * 2 - distance;
+    const minPoint = this.SMALLEST_SPACE;
+
+    return makeRandomNumber(minPoint, maxPoint);
+  }
+}
--- makeVillage.js
+++ makeVillage.js
@@ -1,21 +1,14 @@
 import { isOverlapWithPreTowns } from './isOverlapWithPreTowns.js';
-import { mailboxSizeMaker, townNameMaker } from './villageUtils.js';
+import { Town } from './Town.js';
 
-export const makeVillage = (rootWidth, rootHeight) => {
-  const village = villageMaker(rootWidth, rootHeight);
-  townNameMaker.makeName(village);
-  mailboxSizeMaker.makeAllMailboxSize(village);
-
-  return village;
-};
-
-const villageMaker = (parentWidth = rootWidth, parentHeight = rootHeight) => {
+export const makeVillage = (parentWidth = rootWidth, parentHeight = rootHeight) => {
   const village = [];
   const MAX_TOWN_COUNT = 10;
   let trialCount = 0;
 
   while (village.length < MAX_TOWN_COUNT) {
-    if (trialCount === 3) {
+    const isLastTrial = trialCount === 3;
+    if (isLastTrial) {
       return village;
     }
 
@@ -25,46 +18,9 @@ const villageMaker = (parentWidth = rootWidth, parentHeight = rootHeight) => {
       continue;
     }
 
+    town.setTown();
     village.push(town);
   }
 
   return village;
 };
-
-class Town {
-  constructor(parentWidth, parentHeight) {
-    this.name = '';
-    this.MIN_WIDTH = 100;
-    this.MIN_HEIGHT = 100;
-    this.width = this.makeRandomLength(parentWidth, this.MIN_WIDTH);
-    this.height = this.makeRandomLength(parentHeight, this.MIN_HEIGHT);
-    this.pointX = this.makeRandomPoint(parentWidth, this.width);
-    this.pointY = this.makeRandomPoint(parentHeight, this.height);
-    this.children = this.makeChildren();
-    this.hasMailbox = Math.random() < 0.4;
-    this.mailboxSize = 0;
-  }
-
-  setName(name) {
-    this.name = name;
-  }
-
-  setMailboxSize(size) {
-    this.mailboxSize = size;
-  }
-
-  makeChildren() {
-    if (this.width <= this.MIN_WIDTH || this.height <= this.MIN_HEIGHT) {
-      return;
-    }
-    return villageMaker(this.width, this.height);
-  }
-
-  makeRandomLength(parentLength, minLength) {
-    return Math.floor(Math.random() * (parentLength / 2 - minLength)) + (minLength - 10);
-  }
-
-  makeRandomPoint(parentLength, length) {
-    return Math.floor(Math.random() * (parentLength - 20 - length)) + 10;
-  }
-}
