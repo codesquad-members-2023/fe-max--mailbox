152aeaaf4d30769753a262634c3bb6db5cbfcea3
qkdflrgs <qkdflrgs@gmail.com>
2023-03-30 15:53:19 +0900

    feat: 마을 위치 랜덤 지정 기능 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 const app = () => {
-    printVillages();
+    renderVillages();
 }
 
-const printVillages = () => {
+const renderVillages = () => {
     const randomNumberOfVillage = Math.floor(Math.random() * 3) + 3;
     makeVillage(randomNumberOfVillage);
     getVillageSize();
@@ -23,12 +23,23 @@ const makeVillage = (num) => {
 const getVillageSize = () => {
     const villages = document.querySelectorAll('.village');
     villages.forEach(getRandomSize);
+    villages.forEach(getRandomPosition);
 }
 
 const getRandomSize = (element) => {
-    const randomSize = Math.floor(Math.random() * 300) + 100;
-    element.style.width = `${randomSize}px`;
-    element.style.height = `${randomSize}px`;
+    const randomWidthSize = Math.floor(Math.random() * 200) + 50;
+    const randomHeightSize = Math.floor(Math.random() * 200) + 50;
+    element.style.width = `${randomWidthSize}px`;
+    element.style.height = `${randomHeightSize}px`;
+}
+
+const getRandomPosition = (element) => {
+    const map = document.querySelector('.map');
+    const randomPositionX = Math.floor(Math.random() * (map.offsetWidth - element.offsetWidth));
+    const randomPositionY = Math.floor(Math.random() * (map.offsetHeight - element.offsetHeight));
+
+    element.style.left = `${randomPositionX}px`;
+    element.style.top = `${randomPositionY}px`;
 }
 
 app();
